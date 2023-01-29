d3a39c104015df12192d4c15d7370f1e59fcc2d9
月形拓人 <takuto.tsukigata@meetsmore.com>
2023-01-29 13:40:21 +0900

    Done

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    entry: "./src/index.js",
+
+    output: {
+        path: `${__dirname}/dist`,
+        filename: "bundle.js",
+    },
+    mode: "development",
+
+    devServer: {
+        static: "dist",
+        open: true,
+    }
+};
